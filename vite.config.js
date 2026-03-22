19731c1b9f8c267e333f8e38ccb2b9932a3bda71
Manuele Mion <manuele.mion@gmail.com>
2026-03-22 10:30:27 +0100

    Add Netlify deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   server: {
     cors: {
       origin: "https://www.owlbear.rodeo",
